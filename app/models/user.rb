4cfed4f5375553a3f82d0682bdb0a3940cc0f417
hyoyou <heatherhyou@gmail.com>
2017-08-27 22:07:53 -0400

    add helper methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   has_many :logs
   has_many :meals
-  
+
+  has_secure_password
+  validates :email, presence: { message: "Please enter email address" }
+  validates :password, presence: { message: "Please enter password" }
+
 end
