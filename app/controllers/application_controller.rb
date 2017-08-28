d5d5c21a1a18fae14f329c496bed2d036f4dace6
hyoyou <heatherhyou@gmail.com>
2017-08-27 22:13:32 -0400

    add sign up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,11 +15,11 @@ class ApplicationController < Sinatra::Base
 
   helpers do
     def current_user
-      User.find(session[:user_id]) if logged_in?
+      User.find(session[:id]) if logged_in?
     end
 
     def logged_in?
-      !!session[:user_id]
+      !!session[:id]
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,11 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if logged_in?
+      redirect to ('/logs')
+    else
+      erb :'users/signup'
+    end
+  end
+
 end
