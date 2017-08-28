6d5007430f7880e15fa5f61a617d0c036931b6bf
hyoyou <heatherhyou@gmail.com>
2017-08-27 22:37:49 -0400

    add new meal form

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,3 +1,29 @@
 class MealsController < ApplicationController
 
+  get '/meals' do
+    if logged_in && current_user
+      @user = current_user
+      @meals = Meal.all
+      erb :'/meals/meal_index'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/meals/new' do
+    if current_user
+      erb :'/meals/new_meal'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/meals' do
+    if params[:name] != "" && params[:calories] != ""
+      current_user.meals.create(:name => params[:name], :calories => params[:calories])
+      redirect to '/meals'
+    else
+      redirect to '/meals/new'
+    end
+  end
 end
