a60a349ad5be7ebf5c814c393ed30ca774df4c67
hyoyou <heatherhyou@gmail.com>
2017-08-27 22:19:37 -0400

    add login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,31 @@ class UsersController < ApplicationController
 
   post '/signup' do
     if params[:email] != "" && params[:password] != ""
-      user = User.create(:email => params[:email], :password => params[:password])
-      session[:id] = user.id
+      @user = User.create(:email => params[:email], :password => params[:password])
+      session[:id] = @user.id
       redirect to ('/logs')
     else
       redirect to ('/signup')
     end
   end
+
+  get '/login' do
+    if logged_in?
+      redirect to ('/logs')
+    else
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(:email => params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:id] = @user.id
+      redirect '/logs'
+    else
+      redirect to ('/login')
+    end
+  end
+
   
 end
