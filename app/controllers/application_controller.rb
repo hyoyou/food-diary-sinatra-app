a4bb5cc71bb42024d122f1b08e1e7ff03a7c9df8
hyoyou <heatherhyou@gmail.com>
2017-09-08 09:35:21 -0400

    update session[:id] to session[:user_id] to clarify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,11 +18,11 @@ class ApplicationController < Sinatra::Base
 
   helpers do
     def current_user
-      User.find(session[:id]) if logged_in?
+      User.find(session[:user_id]) if logged_in?
     end
 
     def logged_in?
-      !!session[:id]
+      !!session[:user_id]
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,9 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    if params[:email] != "" && params[:password] != ""
+    if params[:email] != "" && params[:password] != "" && !User.find_by(email: params[:email])
       @user = User.create(:email => params[:email], :password => params[:password])
-      session[:id] = @user.id
+      session[:user_id] = @user.id
       redirect to ('/logs/new')
     else
       flash[:notice] = "Please Make Sure Form Was Filled Out Correctly!"
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
   post '/login' do
     @user = User.find_by(:email => params[:email])
     if @user && @user.authenticate(params[:password])
-      session[:id] = @user.id
+      session[:user_id] = @user.id
       redirect '/logs'
     else
       flash[:notice] = "Please Check Log In Credentials."
